30009e1a64ebee6b6e63e811af29b080c7288c26
Traineratwot <traineratwot@yandex.ru>
2024-06-18 16:15:55 +0300

    fix

--- resources/views/forms/components/map.blade.php
+++ resources/views/forms/components/map.blade.php
@@ -106,7 +106,7 @@
         </x-slot>
     </x-filament::input.wrapper>
     <script type="text/javascript" class="filament-open-street-map">
-      const intervalMap{{ $getId() }} =  setInterval(() => {
+      window['intervalMap{{ $getId() }}'] =  setInterval(() => {
             try {
                 const map = document.getElementById('OSMap-{{ $getId() }}')
                 let disabled = false
@@ -160,7 +160,7 @@
                     }
                 }
             } catch (e) {
-                clearInterval(intervalMap{{ $getId() }})
+                clearInterval(window['intervalMap{{ $getId() }}'])
                 console.warn(e)
             }
         }, 200)
